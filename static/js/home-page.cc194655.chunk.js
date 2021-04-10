2cd682c6fbcccf9acefe079a0da7d783804325d7
Sergey Khalyavin <serg.khalk@gmail.com>
2021-04-10 12:23:33 +0300

    Updates

---
+++ static/js/home-page.cc194655.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[2],{184:function(e,t,o){"use strict";o.r(t);o(0);var c=o(2);t.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)("h1",{children:"Home page"})})}}}]);
+//# sourceMappingURL=home-page.cc194655.chunk.js.map
